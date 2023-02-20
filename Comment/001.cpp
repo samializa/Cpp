0bd454576c81089d767ff33474b7c0171a7a3c70
Samia jannat liza <123775191+samializa@users.noreply.github.com>
2023-02-20 20:49:54 +0600

    Add files via upload

---
+++ Comment/001.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main (){
+int x=20,y=10,sum;
+
+// single line comment
+/*cout<<"Enter the value of sum:"<<endl;
+
+sum=x+y;
+
+cout<<sum;*/
+
+
+
+
+
+}
