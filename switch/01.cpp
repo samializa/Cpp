920f9d2d12ae5df6d69aec63d7af86446b8642c7
Samia jannat liza <123775191+samializa@users.noreply.github.com>
2023-03-04 23:43:51 +0600

    Add files via upload

---
+++ switch/01.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+int main(){
+int color=3;
+switch (color){
+ case 1:
+   cout<<"green:eee"<<endl;
+   break;
+ case 2:
+    cout<<"orange:bba"<<endl;
+    break;
+ case 3:
+    cout<<"sky blue:cse"<<endl;
+    break;
+ default:
+    cout<< "none" <<endl;
+
+
+
+}
+
+return 0;
+
+}
+
