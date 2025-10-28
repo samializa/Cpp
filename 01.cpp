0cb28f3ecdef0304e83c640debdd59d9f5272f5c
Samia jannat liza <123775191+samializa@users.noreply.github.com>
2025-10-28 10:44:33 +0600

    Add files via upload

---
+++ 01.cpp
@@ -0,0 +1,22 @@
+
+#include<iostream>
+using namespace std;
+int main(){
+
+int arr[3][3]={11,12,13,21,22,23,31,32,33};
+
+for(int i=0;i<3;i++){
+        cout<<endl;
+        for(int j=0;j<3;j++){
+
+            cout<<arr[i][j]<<" ";
+        }
+
+}
+
+
+}
+
+
+
+
